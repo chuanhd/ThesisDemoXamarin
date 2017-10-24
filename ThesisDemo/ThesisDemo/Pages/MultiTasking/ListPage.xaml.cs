4fe08ed23b0cb978ad78128c349aa6df0c24ae4c
chuanhd@iMac <chuan_hd@Chuans-iMac-2.local>
2017-10-24 11:16:51 +0700

    Add image list view

--- ThesisDemo/ThesisDemo/Pages/MultiTasking/ListPage.xaml.cs
+++ ThesisDemo/ThesisDemo/Pages/MultiTasking/ListPage.xaml.cs
@@ -8,9 +8,16 @@ namespace ThesisDemo.Pages.MultiTasking
 {
 	public partial class ListPage : ContentPage, IBasePage<ListPageModel>
 	{
+
+        ListPageModel _model = new ListPageModel();
+
 		public ListPage()
 		{
 			InitializeComponent();
+
+            _model.Reload();
+            ImagesListView.ItemsSource = _model.Items;
+
 		}
 	}
 }
